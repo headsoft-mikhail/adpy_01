c3372328738002388ceba0047ee7e28a22a01ad8
headsoft-mikhail <82053882+headsoft-mikhail@users.noreply.github.com>
2021-07-08 13:58:35 +0300

    Add files via upload

---
+++ application/__init__.py
@@ -0,0 +1,3 @@
+from datetime import datetime as dt
+from .salary import calculate_salary
+from .people import get_employees
---
+++ dirty_main.py
@@ -0,0 +1,7 @@
+from application import *
+
+if __name__ == '__main__':
+    print(dt.now().strftime("%Y-%m-%d %H:%M:%S"))
+    calculate_salary()
+    get_employees()
+
